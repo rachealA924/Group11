52d73527a8cf4a3a634c59cbec8e20c33f2a2fb8
Shekinah-Ineza <m.ineza1@alustudent.com>
2025-06-09 06:20:36 -0700

    all

--- js-gpa-calculator/script.js
+++ js-gpa-calculator/script.js
@@ -17,7 +17,14 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   const name = document.getElementById('assignment-name').value;
   const grade = parseFloat(document.getElementById('assignment-grade').value);
-  if (!name || isNaN(grade) || grade < 0 || grade > 5) return;
+  if (!name.trim()) {
+  alert("Assignment name cannot be empty.");
+  return;
+}
+if (isNaN(grade) || grade < 0 || grade > 5) {
+  alert("Please enter a grade between 0 and 5.");
+  return;
+}
 
   const assignment = { name, grade };
   assignments.push(assignment);
